4db7b4fb72293c965dd39bee912e86a04bc66509
Oliver CHIU <lihaotong10@gmail.com>
2020-09-24 09:46:43 +0800

    Update Solution.java

--- 283. Move Zeroes/Solution.java
+++ 283. Move Zeroes/Solution.java
@@ -4,6 +4,7 @@ class Solution {
 		 for(int i = 0;i < nums.length;i++){
 			if(nums[i] != 0){
 				nums[index] = nums[i];
+				//如果非0数与0发生了交换，将交换后的非0数变为0
 				if(index != i){
 					nums[i] = 0;
 				}
